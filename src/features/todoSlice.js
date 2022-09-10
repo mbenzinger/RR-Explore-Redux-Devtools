e6740cdee74b0299115bd5ca5277084a1786598b
Mason Benzinger <mason.benzinger@gmail.com>
2022-09-10 11:38:07 -0500

    commited

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -9,13 +9,13 @@ export const todoSlice = createSlice({
     initialState,
     reducers: {
         addTodo: (state, action) => {
-            return { items: [...state.items, action.payload]}
+            return { items: [...state.items, action.payload] }
         },
         removeOne: (state, action) => {
             console.log(action)
             let array = [...state.items]
             let index = action.payload
-            if(index !== -1) {
+            if (index !== -1) {
                 array.splice(index, 1)
                 return { items: array }
             }
